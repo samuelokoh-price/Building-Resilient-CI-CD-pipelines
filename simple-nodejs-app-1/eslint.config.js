10b11dae1ba003f267a0558a5acd50678226a359
samuelokoh-price <paspagesamm@gmail.com>
2025-12-26 16:57:18 +0100

    added ./eslint.config.mjs

--- simple-nodejs-app-1/eslint.config.js
+++ simple-nodejs-app-1/eslint.config.js
@@ -1,12 +1,21 @@
-// eslint.config.js
 const js = require("@eslint/js");
 
 module.exports = [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        // Node.js globals
+        require: "readonly",
+        module: "readonly",
+        console: "readonly",
+        // Jest globals
+        test: "readonly",
+        expect: "readonly"
+      }
+    },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off"
+      "no-unused-vars": "warn"
     }
   }
 ];
